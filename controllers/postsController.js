255a2fe34efbfbb6d5949a877f541bcb90dd3494
Mattia Mion <mattia.mion@gmail.com>
2026-02-19 17:27:10 +0100

    fix commenti

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -20,16 +20,16 @@ function show(req, res) {
     const id = req.params.id
     const postSql = 'SELECT * FROM posts WHERE id = ?';
     const tagsSql = `SELECT T.* FROM tags T JOIN post_tag PT ON T.id = PT.tag_id WHERE PT.post_id = ?`;
-    // Eseguiamo la prima query per la pizza
+    // Eseguiamo la prima query per il post
     connection.query(postSql, [id], (err, postResults) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (postResults.length === 0) return res.status(404).json({ error: 'Pizza not found' });
-        // Recuperiamo la pizza
+        // Recuperiamo il post
         const post = postResults[0];
-        // Se è andata bene, eseguiamo la seconda query per gli ingredienti
+        // Se è andata bene, eseguiamo la seconda query per i tags
         connection.query(tagsSql, [id], (err, tagsResults) => {
             if (err) return res.status(500).json({ error: 'Database query failed' });
-            // Aggoiungiamo gli ingredienti alla pizza
+            // Aggoiungiamo i tags al post
             post.tags = tagsResults;
             res.json(post);
         });
